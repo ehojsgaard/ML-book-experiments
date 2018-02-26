dcc1a361930fc63e5e2f3a1f602f79a98611247b
Espen Højsgaard <espen.hoejsgaard@keylane.com>
2018-02-26 11:11:45 +0100

    Implement simple model

--- DigitsRecognizer/FSharp/Script.fsx
+++ DigitsRecognizer/FSharp/Script.fsx
@@ -17,3 +17,25 @@ let dataPath = @"/Users/espen/git/ML-book-experiments/DigitsRecognizer/data/"
 let trainingPath = dataPath + "trainingsample.csv"
 let trainingSet = reader trainingPath
 let validationPath = dataPath + "validationsample.csv";
+let validationSet = reader validationPath
+
+
+let manhattanDistance (a1:int[]) (a2:int[]) =
+    Array.zip a1 a2
+    |> Array.map (fun (i1, i2) -> abs (i1 - i2))
+    |> Array.sum
+
+let train (trainingSet:Observation[]) =
+    let classify (pixels:int[]) = 
+        trainingSet
+        |> Array.minBy (fun x -> manhattanDistance x.Pixels pixels)
+        |> fun x -> x.Label
+    classify    
+
+let classifier = train trainingSet
+
+let correctnessPct =
+    validationSet
+    |> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+
+printfn "Correct: %.3f"
